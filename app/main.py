f461a187d086955bff6299a54f3e9f8768fb60c4
ArtemSarafannikov <sarafannikov.artem@yandex.ru>
2025-03-23 11:19:19 +0300

    feat: add test endpoint

--- app/main.py
+++ app/main.py
@@ -8,3 +8,7 @@ API.include_router(ingredients.router, prefix="/api/ingredients")
 API.include_router(recipes.router, prefix="/api/recipes")
 API.include_router(tags.router, prefix="/api/tags")
 API.include_router(users.router, prefix="/api/users")
+
+@API.get("/test")
+def test():
+    return {"test": "test"}
